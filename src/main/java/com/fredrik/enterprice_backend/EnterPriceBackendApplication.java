cc6f90deca8607986b3a8ef9d22825592b8b62e7
HuckerDuck / Fredrik <Frmb1@hotmail.com>
2025-11-19 19:36:00 +0100

    Second commit all added

---
+++ src/main/java/com/fredrik/enterprice_backend/EnterPriceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fredrik.enterprice_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterPriceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterPriceBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fredrik/enterprice_backend/EnterPriceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fredrik.enterprice_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterPriceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

b95560a7d6242e68dff10ca41ca24ed4ce24ee1f
HuckerDuck aka Fredrik <fredrikmenotbrauer@gmail.com>
2025-11-20 23:19:25 +0100

    ## AuthController with Login, Updated SecurityConfig ##
    - Added a LoginMethod for the /api/auth.
    
    - Wanted to check that I can all that I need and that I get a token and that the password is hashed from the start.
    
    - It all seems to work really good so am glad by that I guess.
    
    - Tomorrow we are gonna move onto the TODO-task, do like a One->Many in the database.
    
    - Had to open up in the SecurityConfig so you can reach the login

--- src/main/java/com/fredrik/enterprice_backend/security/SecurityConfig.java
+++ src/main/java/com/fredrik/enterprice_backend/security/SecurityConfig.java
@@ -1,21 +1,41 @@
 package com.fredrik.enterprice_backend.security;
 
+import com.fredrik.enterprice_backend.security.jwt.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        //? Allow people to register on the api
+
+                        //? Opening this up to let people login / register
+                        //? When logging in they get a token back
+                        //? This will be handy for security reason
+                        //? Will match what I will do later in the frontend
+                        .requestMatchers("/api/auth/login").permitAll()
                         .requestMatchers("/api/ducks/register").permitAll()
                         .requestMatchers("/api/ducks").permitAll()
                         .requestMatchers("/api/ducks/**").permitAll()

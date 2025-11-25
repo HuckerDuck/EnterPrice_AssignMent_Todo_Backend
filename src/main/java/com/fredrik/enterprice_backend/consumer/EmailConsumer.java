69d856aa5f3af87d3293ca3eaa8dd50b0a08daba
HuckerDuck / Fredrik <Frmb1@hotmail.com>
2025-11-25 10:21:08 +0100

    ## Adding Docker and Rabbit to development ##
    - Started by adding the dependecy and then I added the boiler plate
    - Running Linux on this computer so removed

---
+++ src/main/java/com/fredrik/enterprice_backend/consumer/EmailConsumer.java
@@ -0,0 +1,16 @@
+package com.fredrik.enterprice_backend.consumer;
+
+import com.fredrik.enterprice_backend.config.RabbitConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+
+public class EmailConsumer {
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @RabbitListener(queues = RabbitConfig.QUEUE_NAME)
+    public void handleMessage(String message) {
+        System.out.println("TESTING");
+        log.info(message);
+    }
+}
